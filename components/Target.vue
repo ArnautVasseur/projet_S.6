2f0cdcb898d266215e973f9cce9ec9050c1a1555
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-01-24 11:33:05 +0100

    Initializing a simple mini-game of point-and-click on target

--- components/Target.vue
+++ components/Target.vue
@@ -1,11 +1,33 @@
 <template>
-  <div id="target"></div>
+  <div class="target" :style="{ left: targetPosition.x + 'px', top: targetPosition.y + 'px', width: targetSize.width + 'px', height: targetSize.height + 'px' }" @click="hitTarget"></div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref } from 'vue';
 
+let targetPosition = ref({ x: 0, y: 0 });
+let targetSize = ref({ width: 50, height: 50 });
+
+function hitTarget() {
+  targetPosition.value.x = Math.random() * 200;
+  targetPosition.value.y = Math.random() * 200;
+  const newSize = getRandomSize();
+  targetSize.value.width = newSize;
+  targetSize.value.height = newSize;
+};
+
+function getRandomSize() {
+  return Math.random() * (100 - 20) + 20; // Adjust the range of random sizes as needed
+};
 </script>
 
-<style>
-
+<style scoped>
+.target {
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  background: radial-gradient(#3223de, #7cc6d1);
+  position: absolute;
+  transition-duration: 300ms;
+}
 </style>
--- pages/ShootThemAll.vue
+++ pages/ShootThemAll.vue
@@ -1,13 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
 <template>
-  
+  <div id="app">
+    <Target v-if="!isGameWon" @click="isGameWon ? null : increaseScore()" />
+    <div v-if="isGameWon">
+      <p>Vous avez gagné!</p>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+let score = ref(0);
+let isGameWon = ref(false);
 
+function increaseScore() {
+  score.value += 1;
+
+  if (score.value === 10) {
+    console.log("Vous avez gagné");
+    isGameWon.value = true;
+  } else {
+    console.log(score.value);
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+}
 </style>
