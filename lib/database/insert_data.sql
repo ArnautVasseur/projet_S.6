f12eb9b128e7997c182d1eae7e7576b58fddaa5e
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-01-25 14:32:34 +0100

    Insert data in database

---
+++ lib/database/insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Achievements(achievement_ID, name, user_ID, obtained)
+VALUES 
+(0, 'Triangle des Bermudes', 1, false),
+(1, 'Triangle du Dragon', 1, false),
+(2, 'DeadZone', 1, false),
+(3, 'MarianaTrench', 1, false),
+(4, 'BalticSeaGhostLights', 1, false),
+(5, 'Submarines1968', 1, false),
+(6, 'MaryCeleste', 1, false),
+(7, 'TheBloop', 1, false),
+(8, 'Cryptids', 1, false)
