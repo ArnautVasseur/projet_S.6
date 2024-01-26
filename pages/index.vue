f5cf4aef85c5af6992f7bb084283aa71c709c818
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-01-26 13:40:31 +0100

    Initialize SCSS and test

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        <p>test</p>
+    <div class="background">
+
     </div>
 </template>
+
+<style lang="scss">
+// .background{
+//     margin: 0px;
+//     width: 100vw;
+//     height: 100vh;
+//     background-size: 100% 100%;
+//     background-position: 0px 0px,0px 0px,0px 0px,0px 0px;
+//     background-image: radial-gradient(20% 50% at 0% 20%, #05CBA569 0%, #073AFF00 100%),radial-gradient(20% 50% at 100% 10%, #05CBA559 0%, #073AFF00 100%),radial-gradient(20% 30% at 30% 1%, #8A4FFF4D 9%, #073AFF00 100%),linear-gradient(320deg, #020015 42%, #291379FF 100%);
+// }
+</style>
