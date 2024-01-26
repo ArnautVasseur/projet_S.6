649ecee1f59feb83fb298703b9ad2083ca1334c5
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-01-26 10:07:11 +0100

    Changing input method by user and adding functionnalities to mini-game

--- pages/MiniTAS.vue
+++ pages/MiniTAS.vue
@@ -2,16 +2,35 @@
   <div id="app">
     <div class="grid">
       <div v-for="(row, rowIndex) in map" :key="rowIndex" class="row">
-        <div v-for="(cell, colIndex) in row" :key="colIndex" class="cell" :class="{ submarine: isSubmarine(rowIndex, colIndex) }">
+        <div
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          class="cell"
+          :class="{ submarine: isSubmarine(rowIndex, colIndex) }"
+        >
           {{ cell }}
         </div>
       </div>
     </div>
 
     <div class="controls">
-      <label for="instructions">Enter Instructions:</label>
-      <input v-model="instructionInput" type="text" id="instructions" />
-      <button @click="executeInstructions">Move Submarine</button>
+      <label for="instructions">Select Instructions:</label>
+      <select v-model="selectedInstruction" id="instructions">
+        <option value="W">West</option>
+        <option value="S">South</option>
+        <option value="E">East</option>
+        <option value="N">North</option>
+      </select>
+      <button @click="addInstruction">Add Instruction</button>
+
+      <div v-if="instructionList.length > 0">
+        <p>Selected Instructions:</p>
+        <ul>
+          <li v-for="(instruction, index) in instructionList" :key="index">{{ instruction }}</li>
+        </ul>
+        <button @click="executeInstructions">Move Submarine</button>
+        <button @click="clearInstructions">Clear Instructions</button>
+      </div>
     </div>
   </div>
 </template>
@@ -30,51 +49,74 @@ export default {
         ["X", " ", " ", "X", "X", " ", " ", " ", " ", "X"],
         ["X", "X", " ", " ", "X", " ", "X", "X", " ", "X"],
         ["X", " ", "X", " ", " ", " ", "X", " ", "X", "X"],
-        ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"]
+        ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
       ],
       Start: [1, 1],
       SubMarine: [1, 1],
       End: [8, 2],
-      instructionInput: "",
-      Instructions: [],
+      selectedInstruction: "",
+      instructionList: [],
     };
   },
   methods: {
-    executeInstructions() {
-      this.Instructions = this.parseInstructions();
-      this.moveSubmarine();
+    addInstruction() {
+      if (this.selectedInstruction) {
+        this.instructionList.push(this.selectedInstruction);
+      }
     },
-    parseInstructions() {
-      // Assuming the input will be a string containing only N, E, S, W
-      return this.instructionInput.split("").map((char) => {
-        switch (char.toUpperCase()) {
-          case "W":
-            return [-1, 0];
-          case "S":
-            return [0, 1];
-          case "E":
-            return [1, 0];
-          case "N":
-            return [0, -1];
-          default:
-            return [0, 0]; // Ignore invalid characters
-        }
+    clearInstructions() {
+      this.instructionList = [];
+    },
+    async executeInstructions() {
+      for (let i = 0; i < this.instructionList.length; i++) {
+        await this.moveSubmarineAsync(this.instructionList[i]);
+      }
+
+      // Check if the submarine has reached the end
+      if (
+        this.SubMarine[0] !== this.End[0] ||
+        this.SubMarine[1] !== this.End[1]
+      ) {
+        // Reposition the submarine to the start
+        this.SubMarine = [...this.Start];
+        this.clearInstructions();
+        console.log("Submarine repositioned to the start!");
+      }
+    },
+    async moveSubmarineAsync(instruction) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const movement = this.parseInstruction(instruction);
+          let nextTile = [
+            this.SubMarine[0] + movement[0],
+            this.SubMarine[1] + movement[1],
+          ];
+
+          if (this.map[nextTile[0]][nextTile[1]] == " ") {
+            this.SubMarine[0] += movement[0];
+            this.SubMarine[1] += movement[1];
+          } else if (this.map[nextTile[0]][nextTile[1]] == "E") {
+            console.log("Vous avez atteint la fin !");
+          } else {
+            console.log("Vous avez rencontré un mur !");
+          }
+
+          resolve();
+        }, 500); // Adjust the delay (in milliseconds) as needed
       });
     },
-    moveSubmarine() {
-      for (let i = 0; i < this.Instructions.length; i++) {
-        let nextTile = [this.SubMarine[0] + this.Instructions[i][0], this.SubMarine[1] + this.Instructions[i][1]];
-
-        if (this.map[nextTile[0]][nextTile[1]] == " ") {
-          this.SubMarine[0] += this.Instructions[i][0];
-          this.SubMarine[1] += this.Instructions[i][1];
-        } else if (this.map[nextTile[0]][nextTile[1]] == "E") {
-          console.log("Vous avez atteint la fin !");
-          return;
-        } else {
-          console.log("Vous avez rencontré un mur !");
-          return;
-        }
+    parseInstruction(instruction) {
+      switch (instruction.toUpperCase()) {
+        case "W":
+          return [-1, 0];
+        case "S":
+          return [0, 1];
+        case "E":
+          return [1, 0];
+        case "N":
+          return [0, -1];
+        default:
+          return [0, 0];
       }
     },
     isSubmarine(row, col) {
