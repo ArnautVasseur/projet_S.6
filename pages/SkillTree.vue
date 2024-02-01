8b09f612e1aac5a15cf415466eddac0f4e3fd2eb
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-02-01 14:12:23 +0100

    add achievement to achievement table when the user unlocks an element

--- pages/SkillTree.vue
+++ pages/SkillTree.vue
@@ -3,49 +3,49 @@
         <div class="main__1">
             <div class="SkillTree">
                 <div class="SkillTree__Start">
-                    <div class="SkillTree__Item__Départ" @click="checkbox(Achievements[0])">
+                    <div class="SkillTree__Item__Départ" :class="{ 'checked': Achievements[0].checked.value }" @click="checkbox(Achievements[0])">
                         <p>{{ Achievements[0].Name }}</p>
                         <p>{{ Achievements[0].checked }}</p>
                     </div>
-                    <div class="SkillTree__Item__Bermudes" @click="checkbox(Achievements[1])">
+                    <div class="SkillTree__Item__Bermudes" :class="{ 'checked': Achievements[1].checked.value }" @click="checkbox(Achievements[1])">
                         <p>{{ Achievements[1].Name }}</p>
                         <p>{{ Achievements[1].checked }}</p>
                     </div>
                 </div>
                 <div class="SkillTree__Locations">
-                    <div class="SkillTree__Item__Dragon" @click="checkbox(Achievements[7])">
+                    <div class="SkillTree__Item__Dragon" :class="{ 'checked': Achievements[7].checked.value }" @click="checkbox(Achievements[7])">
                         <p>{{ Achievements[7].Name }}</p>
                         <p>{{ Achievements[7].checked }}</p>
                     </div>
-                    <div class="SkillTree__Item__DeadZone" @click="checkbox(Achievements[9])">
+                    <div class="SkillTree__Item__DeadZone" :class="{ 'checked': Achievements[9].checked.value }" @click="checkbox(Achievements[9])">
                         <p>{{ Achievements[9].Name }}</p>
                         <p>{{ Achievements[9].checked }}</p>
                     </div>
-                    <div class="SkillTree__Item__MarianaTrench" @click="checkbox(Achievements[8])">
+                    <div class="SkillTree__Item__MarianaTrench" :class="{ 'checked': Achievements[8].checked.value }" @click="checkbox(Achievements[8])">
                         <p>{{ Achievements[8].Name }}</p>
                         <p>{{ Achievements[8].checked }}</p>
                     </div>
                 </div>
                 <div class="SkillTree__Disappearances">
-                    <div class="SkillTree__Item__GhostLights" @click="checkbox(Achievements[2])">
+                    <div class="SkillTree__Item__GhostLights" :class="{ 'checked': Achievements[2].checked.value }" @click="checkbox(Achievements[2])">
                         <p>{{ Achievements[2].Name }}</p>
                         <p>{{ Achievements[2].checked }}</p>
                     </div>
-                    <div class="SkillTree__Item__MaryCeleste" @click="checkbox(Achievements[4])">
+                    <div class="SkillTree__Item__MaryCeleste" :class="{ 'checked': Achievements[4].checked.value }" @click="checkbox(Achievements[4])">
                         <p>{{ Achievements[4].Name }}</p>
                         <p>{{ Achievements[4].checked }}</p>
                     </div>
-                    <div class="SkillTree__Item__SubMarines" @click="checkbox(Achievements[3])">
+                    <div class="SkillTree__Item__SubMarines" :class="{ 'checked': Achievements[3].checked.value }" @click="checkbox(Achievements[3])">
                         <p>{{ Achievements[3].Name }}</p>
                         <p>{{ Achievements[3].checked }}</p>
                     </div>
                 </div>
                 <div class="SkillTree__Creatures">
-                    <div class="SkillTree__Item__Bloop" @click="checkbox(Achievements[5])">
+                    <div class="SkillTree__Item__Bloop" :class="{ 'checked': Achievements[5].checked.value }" @click="checkbox(Achievements[5])">
                         <p>{{ Achievements[5].Name }}</p>
                         <p>{{ Achievements[5].checked }}</p>
                     </div>
-                    <div class="SkillTree__Item__Cryptids" @click="checkbox(Achievements[6])">
+                    <div class="SkillTree__Item__Cryptids" :class="{ 'checked': Achievements[6].checked.value }" @click="checkbox(Achievements[6])">
                         <p>{{ Achievements[6].Name }}</p>
                         <p>{{ Achievements[6].checked }}</p>
                     </div>
@@ -95,9 +95,12 @@ const getAchievement = async () => {
     const userAchievements = response.data.map((ach: { name: string }) => ach.name);
     console.log(userAchievements)
 
-    // Update the Achievements array to set checked to true if the achievement is in the user's achievements
     Achievements.forEach((ach) => {
-        ach.checked.value = userAchievements.includes(ach.Name);
+        if (ach.Name === "Départ") {
+            ach.checked.value = true;
+        } else {
+            ach.checked.value = userAchievements.includes(ach.Name);
+        }
         console.log(ach.checked.value)
     });
 };
@@ -110,6 +113,7 @@ onMounted(async () => {
 
 let Achievements = [
     {
+        achievement_ID : 0,
         ID: "SkillTree_0",
         Name: "Départ",
         Tooltip: "Cette partie vous est gratuite",
@@ -117,6 +121,7 @@ let Achievements = [
         parents:[]
     },
     {
+        achievement_ID : 1,
         ID: "SkillTree_1",
         Name: "Triangle Des Bermudes",
         Tooltip: "Cette partie vous est gratuite, clickez ici pour la débloquer",
@@ -124,6 +129,7 @@ let Achievements = [
         parent:["SkillTree_0"],
     },
     {
+        achievement_ID : 2,
         ID: "SkillTree_2",
         Name: "Vaisseaux fantômes de la mer Baltique",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -131,6 +137,7 @@ let Achievements = [
         parent:["SkillTree_1"],
     },
     {
+        achievement_ID : 3,
         ID: "SkillTree_3",
         Name: "Disparitions Sous-Marins 1968",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -138,6 +145,7 @@ let Achievements = [
         parent:["SkillTree_2"],
     },
     {
+        achievement_ID : 4,
         ID: "SkillTree_4",
         Name: "Mary Celeste",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -145,6 +153,7 @@ let Achievements = [
         parent:["SkillTree_2"],
     },
     {
+        achievement_ID : 5,
         ID: "SkillTree_5",
         Name: "The Bloop",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -152,6 +161,7 @@ let Achievements = [
         parent:["SkillTree_1"],
     },
     {
+        achievement_ID : 6,
         ID: "SkillTree_6",
         Name: "Les Cryptides Sous-Marines",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -159,6 +169,7 @@ let Achievements = [
         parent:["SkillTree_5"],
     },
     {
+        achievement_ID : 7,
         ID: "SkillTree_7",
         Name: "Triangle Du Dragon",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -166,6 +177,7 @@ let Achievements = [
         parent:["SkillTree_1"],
     },
     {
+        achievement_ID : 8,
         ID: "SkillTree_8",
         Name: "La fosse Mariane",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -173,6 +185,7 @@ let Achievements = [
         parent:["SkillTree_9", "SkillTree_7"],
     },
     {
+        achievement_ID : 9,
         ID: "SkillTree_9",
         Name: "Golf du Mexique, 'Dead Zone'",
         Tooltip: "Débloquez cette partie du site pour 1 Point de Compétence",
@@ -186,6 +199,7 @@ interface AchievementItem {
 }
 
 interface Achievement {
+    achievement_ID : number;
     ID: string;
     Name: string;
     Tooltip: string;
@@ -193,97 +207,38 @@ interface Achievement {
     parent?: string[];
 }
 
-function checkbox(skill: Achievement) {
-    if(Points.value <= 0){
+async function checkbox(skill: Achievement) {
+    if (Points.value <= 0) {
         console.log("Vous n'avez plus assez de points");
         errormessage.value = true;
-    }
-    else{
+    } else {
         if (skill.checked.value === false) {
-        // Check if any parent is checked
-        if (skill.parent && skill.parent.some(parentId => Achievements.find(parent => parent.ID === parentId)?.checked.value)) {
-            // Display confirmation dialog
-            if (window.confirm(`Are you sure you want to check ${skill.Name}?`)) {
-                const NewPoints = Points.value - 1;
-                updatePoints(store.token, NewPoints);
-                Points.value = NewPoints;
-                console.log("points: " + Points.value);
-                skill.checked.value = true;
+            // Check if any parent is checked
+            if (skill.parent && skill.parent.some(parentId => Achievements.find(parent => parent.ID === parentId)?.checked.value)) {
+                // Display confirmation dialog
+                if (window.confirm(`Are you sure you want to unlock ${skill.Name}?`)) {
+                    const NewPoints = Points.value - 1;
+                    updatePoints(store.token, NewPoints);
+                    Points.value = NewPoints;
+                    console.log("points: " + Points.value);
+                    skill.checked.value = true;
 
-                const bermudesItem = document.querySelector('.SkillTree__Item__Bermudes');
-                const ghostlightItem = document.querySelector('.SkillTree__Item__GhostLights');
-                const submarineItem = document.querySelector('.SkillTree__Item__SubMarines');
-                const dragonItem = document.querySelector('.SkillTree__Item__Dragon');
-                const marycelesteItem = document.querySelector('.SkillTree__Item__MaryCeleste');
-                const bloopItem = document.querySelector('.SkillTree__Item__Bloop');
-                const cryptidsItem = document.querySelector('.SkillTree__Item__Cryptids');
-                const marianatrenchItem = document.querySelector('.SkillTree__Item__MarianaTrench');
-                const deadzoneItem = document.querySelector('.SkillTree__Item__DeadZone');
+                    // Add achievement to the database
+                    await addAchievement(store.token, skill.Name);
 
-                switch(skill.ID){
-                    case 'SkillTree_1':
-                            if (bermudesItem) {
-                                bermudesItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_2':
-                            if (ghostlightItem) {
-                                ghostlightItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_3':
-                            if (submarineItem) {
-                                submarineItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_4':
-                            if (marycelesteItem) {
-                                marycelesteItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_5':
-                            if (bloopItem) {
-                                bloopItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_6':
-                            if (cryptidsItem) {
-                                cryptidsItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_7':
-                            if (dragonItem) {
-                                dragonItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_8':
-                            if (marianatrenchItem) {
-                                marianatrenchItem.classList.add('checked');
-                            }
-                        break;
-
-                    case 'SkillTree_9':
-                            if (deadzoneItem) {
-                                deadzoneItem.classList.add('checked');
-                            }
-                        break;
+                    // Update the corresponding DOM element
+                    const skillElement = document.querySelector(`.SkillTree__Item__${skill.Name.replace(/\s+/g, '')}`);
+                    if (skillElement) {
+                        skillElement.classList.add('checked');
+                    }
                 }
+            } else {
+                console.log("Cannot click because one of the parents is not checked");
             }
         } else {
-            console.log("Cannot click because one of the parents is not checked");
+            console.log("Case déjà cochée");
         }
-    } else {
-        console.log("Case déjà cochée");
     }
-    }
-    
 }
 
 const updatePoints = async (userID: number | null, NewPoints: number) => {
@@ -295,12 +250,17 @@ const updatePoints = async (userID: number | null, NewPoints: number) => {
     }
 };
 
-const addAchievement = async (userID: number | null, NewAchievement: number) => {
+const addAchievement = async (userID: number | null, Name: string ) => {
     try {
-        const response = await API.put(`/users/${userID}/achievement/${NewAchievement}`, { achievement: NewAchievement });
+        if (userID === null) {
+            console.error("User ID is null");
+            return;
+        }
+
+        const response = await API.post(`/users/${userID}/achievement/${Name}`);
         console.log("Achievement added successfully:", response.data);
     } catch (error) {
-        console.error("Error updating points:", error);
+        console.error("Error adding achievement:", error);
     }
 };
 
@@ -344,7 +304,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 50%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__Bermudes{
@@ -355,7 +317,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 50%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__Dragon{
@@ -366,7 +330,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 27.5%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__DeadZone{
@@ -377,7 +343,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 27.5%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__MarianaTrench{
@@ -388,7 +356,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 10%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__GhostLights{
@@ -399,7 +369,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 72.5%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__MaryCeleste{
@@ -410,7 +382,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 90%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__SubMarines{
@@ -421,7 +395,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 90%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__Bloop{
@@ -432,7 +408,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 50%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
 
         &__Cryptids{
@@ -443,7 +421,9 @@ const addAchievement = async (userID: number | null, NewAchievement: number) => 
             left: 72.5%;
             transform: translate(-50%, -50%);
 
-            
+            &.checked {
+                border: 5px solid white;
+            }
         }
     }
 
