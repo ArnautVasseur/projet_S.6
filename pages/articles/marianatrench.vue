8aff82ad728f35b8dcb40d1a04ba7f2b043951ec
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-02-08 10:06:52 +0100

    add marianatranch article

--- pages/articles/marianatrench.vue
+++ pages/articles/marianatrench.vue
@@ -1,10 +1,56 @@
 <template>
   <Comp_Header class="header"></Comp_Header>
+  <div v-if="store.achievement__5" class="main">
+        <div class="main__presentation">
+            La Fosse Mariane
+        </div>
+        <div class="main__article">
+            <div class="main__article__introduction">
+                <div class="main__article__introduction__content">
+                    <div class="main__article__introduction__content__texte">
+                        <h2>À la Découverte de la Fosse Mariane</h2>
+                        <p>Au cœur de l'océan Pacifique, la Fosse Mariane défie les limites de ce que nous connaissons sur les profondeurs marines.<br/><br/> Cette énigmatique fosse océanique, la plus profonde de la planète, abrite des mystères fascinants et des formes de vie uniques.<br/><br/> Dans cet article, nous explorerons les mystères de la Fosse Mariane, plongeant dans ses abysses pour découvrir les secrets qui se cachent dans l'obscurité totale de ses eaux profondes.</p>
+                    </div>
+                    <div class="main__article__introduction__content__images">
+                        <img src="/images/marianatrench3.jpg" alt="marianatrench3">
+                    </div>
+                </div>
+            </div>
+            <div class="main__article__content">
+                <div class="main__article__content__discovery">
+                    <img src="/images/marianatrench2.avif" alt="marianatrench2">
+                    <div class="main__article__content__discovery__texte">
+                        <h2>Un Abîme Profond</h2>
+                        <p>La Fosse Mariane, située à l'est des Philippines, atteint une profondeur remarquable de près de 11 000 mètres, ce qui en fait le point le plus profond de l'océan.<br/><br/> Son abîme insondable résulte de la subduction de la plaque tectonique pacifique sous la plaque philippine, créant une tranchée océanique unique.<br/><br/> Cette profondeur extrême et l'obscurité totale créent un environnement d'une pression inimaginable, défiant la vie telle que nous la connaissons.<br/><br/> Malgré les conditions inhospitalières, la Fosse Mariane abrite une vie marine étonnamment adaptée.<br/><br/> Des créatures fascinantes, telles que le poisson abyssal, la crevette amphipode et des espèces de méduses bioluminescentes, ont évolué pour prospérer dans ces abysses.<br/><br/> Leur capacité à résister à la pression extrême et à survivre dans l'obscurité totale offre des perspectives uniques sur la biologie marine et l'adaptation évolutive.</p>
+                    </div>
+                </div>
+                <div class="main__article__content__theory">
+                    <h2 class="main__article__content__theory__title">Exploration des Profondeurs</h2>
+                    <div class="main__article__content__theory__texte">
+                        <p>La Fosse Mariane a été le site d'explorations intenses au fil des ans.<br/><br/> En 1960, le bathyscaphe Trieste, piloté par Jacques Piccard et Don Walsh, a réussi la première plongée humaine réussie dans ses profondeurs abyssales.<br/><br/> Depuis lors, des sous-marins télécommandés et des technologies de pointe ont permis d'explorer davantage la fosse, révélant des paysages sous-marins spectaculaires et des formes de vie uniques.<br/><br/> Malgré les avancées technologiques, la Fosse Mariane conserve des mystères.<br/><br/> Certains scientifiques s'interrogent sur la possibilité d'une vie microbienne extraterrestre dans ses profondeurs, tandis que d'autres explorent les liens entre les conditions extrêmes de la fosse et les phénomènes sismiques régionaux.<br/><br/> Les mystères de la Fosse Mariane invitent à des enquêtes continues et à une compréhension plus approfondie des processus qui régissent les tréfonds de l'océan.</p>
+                        <p>La Fosse Mariane revêt une importance scientifique cruciale.<br/><br/> En plus de fournir un terrain d'étude unique pour la biologie marine, elle offre des perspectives sur les processus géologiques en jeu dans la subduction des plaques tectoniques.<br/><br/> Comprendre la Fosse Mariane pourrait également offrir des indications sur la biodiversité des environnements extrêmes et la recherche de vie dans d'autres parties de notre système solaire.<br/><br/> Alors que la technologie nous permet d'explorer les profondeurs de la Fosse Mariane, il est essentiel de préserver cet écosystème unique.<br/><br/> Les activités humaines, telles que l'exploitation minière en eaux profondes, pourraient avoir des conséquences dévastatrices sur ces environnements fragiles.<br/><br/> La Fosse Mariane souligne l'importance de l'équilibre entre exploration scientifique et conservation environnementale.</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="main__article__conclusion">
+            <div class="main__article__conclusion__content">
+                <div class="main__article__conclusion__content__texte">
+                    <h2>Conclusion</h2>
+                    <p>La Fosse Mariane demeure l'un des endroits les plus mystérieux et fascinants de notre planète.<br/><br/> Alors que la technologie nous permet d'explorer ses abysses, chaque plongée révèle de nouveaux secrets sur la vie marine et les processus géologiques qui façonnent notre monde.<br/><br/> La préservation de cet écosystème unique est cruciale pour les générations futures, car la Fosse Mariane continue d'inspirer la quête humaine de compréhension et d'exploration des profondeurs inexplorées de l'océan.</p>
+                </div>
+                <div class="main__article__conclusion__content__images">
+                    <img src="/creatures/creature_14.jpg" alt="creature_14">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else>NOPE</div>
+    <Comp_Footer/>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { onMounted } from 'vue';
 const store = useGlobalStore()
 
 //renvoie l'user sur l'index s'il n'est pas connecté
@@ -20,10 +66,9 @@ const getAchievement = async () => {
     const response = await API.get(`/achievements/${store.token}`);
     achievement.value = response.data
     const userAchievements = response.data.map((ach: { name: string }) => ach.name);
-
     
     
-    achievement.value.forEach((succes) =>{
+    achievement.value.forEach((succes: { name: string; }) =>{
         if(succes.name == "Triangle Des Bermudes"){
             store.achievement__1 = true;
         }
@@ -61,6 +106,152 @@ onMounted(async () => {
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.main{
+    color: white;
+    font-family: $montserrat, "sans-serif";
+
+    &__presentation{
+        background-image: 
+        url('/altered_creatures/creature_8_opacity.png'),
+        radial-gradient(20% 50% at 0% 20%, #05CBA569 0%, #073AFF00 100%),
+        radial-gradient(20% 50% at 100% 10%, #05CBA559 0%, #073AFF00 100%),
+        radial-gradient(20% 30% at 30% 1%, #8A4FFF4D 9%, #073AFF00 100%),
+        linear-gradient(320deg, #020015 42%, #291379FF 100%);
+        background-position: bottom;
+        background-size: 120%;
+        background-repeat: no-repeat;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+        padding: 130px 0px;
+
+        @include Article_Title
+    }
+
+    &__article{
+
+        
+        &__conclusion, &__introduction{
+            padding: 100px;
+            background: $primary-color;
+
+            &__content{
+                display: flex;
+                align-content: center;
+                align-items: center;
+                gap: 100px;
+
+                &__images{
+                    position: relative;
+                    margin: auto;
+
+                    img{
+                        width: 100%;
+                        margin: auto;
+                        box-shadow: 0px 0px 50px $teal-color;
+                    }
+                }
+
+                &__texte{
+                    height: 100%;
+                    line-height: 1.5rem;
+                    display: flex;
+                    flex-direction: column;
+                    gap: 30px;
+
+                    h2{
+                        @include h2;
+                        line-height: 40px;
+                        text-align: center;
+                    }
+                }
+            }
+        }
+
+        &__conclusion{
+            background-size: 100% 100%;
+            background-position: 0px 0px,0px 0px,0px 0px;
+            background-image: radial-gradient(50% 50% at -12% 40%, #00FFFF45 3%, #00031700 100%),radial-gradient(34% 46% at 107% 40%, #00FFFF3D 4%, #00031700 100%),radial-gradient(75% 75% at 50% 50%, #0B0D3AFF 0%, #080A1EFF 100%);
+        }
+
+        &__content{
+            background-size: 100% 100%;
+            background-position: 0px 0px,0px 0px,0px 0px;
+            background-image: radial-gradient(50% 50% at -12% 40%, #00FFFF45 3%, #00031700 100%),radial-gradient(34% 46% at 107% 40%, #00FFFF3D 4%, #00031700 100%),radial-gradient(75% 75% at 50% 50%, #0B0D3AFF 0%, #080A1EFF 100%);
+            padding-top: 50px;
+
+            &__theory{
+                width: 100%;
+                padding: 100px;
+                margin-top: 100px;
+                background-size: 100%;
+                background-position: center;
+                background-image: 
+                    url('/altered_pictures/marianatrench_opacity.png'),
+                    radial-gradient(50% 50% at -12% 40%, #00FFFF45 3%, #00031700 100%),
+                    radial-gradient(34% 46% at 107% 40%, #00FFFF3D 4%, #00031700 100%),
+                    radial-gradient(75% 75% at 50% 50%, #0B0D3AFF 0%, #080A1EFF 100%);
+                background-repeat: no-repeat;
+
+                
+                &__title{
+                    @include h2;
+                    text-align: center;
+                    margin-bottom: 100px;
+                }
+
+                &__texte{
+                    display: flex;
+                    justify-content: center;
+                    gap: 50px;
+                    line-height: 20px;
+
+                    p{
+                        width: 100%;
+                    }
+                }
+            }
+
+            &__discovery{
+                padding: 50px 100px 0px 100px;
+                border-top: 5px ;
+                display: flex;
+                align-content: center;
+                align-items: center;
+                gap: 100px;
+
+                &__texte{
+                    width: 70%;
+                    display: flex;
+                    flex-direction: column; 
+                    gap: 30px;
+
+                    h2{
+                        @include h2;
+                        text-align: center;
+                    }
+
+                    p{
+                        line-height: 1.5rem;
+                    }
+                }
+
+                img{
+                    width: 50%;
+                    margin: auto;
+                    box-shadow: 0px 0px 50px $teal-color;
+                }
+            }
+        }
+    }
+}
+
+img{
+    box-shadow: 0px 0px 50px $teal-color;
+}
 
 </style>
