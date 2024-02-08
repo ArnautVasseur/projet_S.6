17957b74f69cb12daa164642d5dd9b4421bbfb5b
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-02-08 15:02:22 +0100

    add ghostship article

--- pages/articles/ghostships.vue
+++ pages/articles/ghostships.vue
@@ -1,11 +1,93 @@
 <template>
   <Comp_Header class="header"></Comp_Header>
+  <div v-if="store.achievement__5" class="main">
+        <div class="main__presentation">
+            Les Vaisseaux Fantômes de la Mer Baltique
+        </div>
+        <div class="main__article">
+
+            <div class="main__article__introduction">
+                <div class="main__article__introduction__content">
+                    <div class="main__article__introduction__content__images">
+                        <img src="/images/ghostship4.avif" alt="ghostship4">
+                    </div>
+                    <div class="main__article__introduction__content__texte">
+                        <h2>Introduction</h2>
+                        <p>La Mer Baltique, riche en histoire et en légendes, est le théâtre de récits fascinants sur des vaisseaux fantômes.<br/><br/> Ces mystérieuses histoires ont perduré à travers les siècles, suscitant l'imagination des marins et des chercheurs.<br/><br/> Dans cet article, nous plongerons dans l'univers des vaisseaux fantômes de la Mer Baltique, explorant les récits légendaires et les réalités mystérieuses qui persistent encore aujourd'hui.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main__article__content">
+
+                <div class="main__article__content__ships">
+
+                    <div class="main__article__content__ships__box">
+                        <div class="main__article__content__ships__box__texte">
+                            <h2>Les Légendes de la Mer Baltique</h2>
+                            <p>La Mer Baltique, bordée par plusieurs pays aux traditions maritimes riches, est imprégnée de légendes de vaisseaux fantômes.<br/><br/> L'une des histoires les plus célèbres est celle du Hollandais Volant, un navire maudit condamné à errer éternellement sur les eaux.<br/><br/> Les récits de marins rapportent avoir vu ses voiles spectrales à l'horizon, présageant souvent des tempêtes dévastatrices.</p>
+                            <span class="border_btm"></span>
+                        </div>
+                        <img src="/images/ghostship5.png" alt="ghostship5">
+                    </div>
+
+                    <div class="main__article__content__ships__box">
+                        <img class="vasa" src="/images/ghostship3.avif" alt="ghostship3">
+                        <div class="main__article__content__ships__box__texte">
+                            <h2>Le Spectre du Vasa</h2>
+                            <p>Un des vaisseaux les plus célèbres de la Mer Baltique, le Vasa, a lui-même acquis une réputation de vaisseau fantôme.<br/><br/> Construit au XVIIe siècle pour le roi suédois Gustave II Adolphe, le Vasa a sombré lors de son voyage inaugural en 1628.<br/><br/> Au fil des siècles, des marins ont prétendu voir le Vasa ressurgir des profondeurs, hantant les eaux avec son histoire tragique.</p>
+                            <span class="border_btm"></span>
+                        </div>
+                    </div>
+
+                    <div class="main__article__content__ships__box">
+                        <div class="main__article__content__ships__box__texte">
+                            <h2>La Malédiction des Voiles Blanches</h2>
+                            <p>Certains récits de vaisseaux fantômes de la Mer Baltique parlent d'une malédiction associée aux voiles blanches.<br/><br/> Selon la légende, les marins apercevant un navire aux voiles immaculées seraient condamnés à rencontrer un destin funeste.<br/><br/> Cette croyance a contribué à l'atmosphère mystique entourant certains navires qui apparaissent soudainement puis disparaissent sans laisser de trace.</p>
+                            <span class="border_btm"></span>
+                        </div>
+                        <img src="/images/ghostship.avif" alt="ghostship">
+                    </div>
+                </div>
+
+                <div class="main__article__content__discovery">
+                    <img src="/images/ghostship2.avif" alt="ghostship2">
+                    <div class="main__article__content__discovery__texte">
+                        <h2>La Réalité des Vaisseaux Submergés</h2>
+                        <p>Au-delà des légendes, la Mer Baltique renferme de véritables vaisseaux submergés, figés dans le temps.<br/><br/> L'un des exemples les plus connus est le Vasa, découvert au fond de la baie de Stockholm en 1956.<br/><br/> Les conditions uniques de la Mer Baltique, caractérisées par une faible teneur en sel, ont préservé de nombreux navires anciens, créant une sorte de musée sous-marin.<br/><br/> La préservation exceptionnelle des épaves dans la Mer Baltique a ouvert la voie à des découvertes énigmatiques.<br/><br/> Des navires datant de plusieurs siècles ont été retrouvés avec leurs cargaisons intactes, offrant un aperçu fascinant de la vie maritime passée.<br/><br/> Cependant, les raisons de leur naufrage restent souvent obscures, alimentant le mystère qui entoure ces vestiges.</p>
+                    </div>
+                </div>
+
+                <div class="main__article__content__theory">
+                    <h2 class="main__article__content__theory__title">Théories et Spéculations</h2>
+                    <div class="main__article__content__theory__texte">
+                        <p>Les avancées technologiques modernes, telles que la sonar et les véhicules sous-marins télécommandés, permettent désormais d'explorer les fonds marins de la Mer Baltique avec une précision inégalée.<br/><br/> Ces outils ont permis de cartographier et d'étudier de manière approfondie les épaves, ouvrant de nouvelles perspectives sur l'histoire maritime de la région.</p>
+                        <p>La préservation du riche patrimoine sous-marin de la Mer Baltique est devenue une préoccupation majeure.<br/><br/> Les autorités et les chercheurs s'efforcent de protéger ces vestiges historiques tout en continuant d'enquêter sur les mystères qu'ils renferment.<br/><br/> La collaboration internationale vise à préserver ce patrimoine unique pour les générations futures.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        
+        <div class="main__article__conclusion">
+            <div class="main__article__conclusion__content">
+                <div class="main__article__conclusion__content__texte">
+                    <h2>Conclusion</h2>
+                    <p>Les vaisseaux fantômes de la Mer Baltique, qu'ils soient issus de légendes séculaires ou de découvertes archéologiques, continuent de captiver notre imagination.<br/><br/> Entre réalité et mystère, ces histoires reflètent la profondeur de l'héritage maritime de la région.<br/><br/> Alors que la technologie moderne éclaire les mystères du passé, la Mer Baltique demeure une mer de légendes et d'énigmes, où les vaisseaux fantômes continuent de naviguer dans les récits captivants des marins d'hier et d'aujourd'hui.</p>
+                </div>
+                <div class="main__article__conclusion__content__images">
+                    <img src="/creatures/creature_15.jpg" alt="creature_15">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else>NOPE</div>
+    <Comp_Footer/>
 </template>
 
 <script setup lang="ts">
 
-import { ref } from 'vue';
-import { onMounted } from 'vue';
+
 const store = useGlobalStore()
 
 //renvoie l'user sur l'index s'il n'est pas connecté
@@ -24,7 +106,7 @@ const getAchievement = async () => {
 
     
     
-    achievement.value.forEach((succes) =>{
+    achievement.value.forEach((succes: { name: string; }) =>{
         if(succes.name == "Triangle Des Bermudes"){
             store.achievement__1 = true;
         }
@@ -62,6 +144,354 @@ onMounted(async () => {
 
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+.main{
+    color: white;
+    font-family: $montserrat, "sans-serif";
+
+    &__presentation{
+        background-image: 
+        url('/altered_creatures/creature_16_opacity.png'),
+        radial-gradient(20% 50% at 0% 20%, #05CBA569 0%, #073AFF00 100%),
+        radial-gradient(20% 50% at 100% 10%, #05CBA559 0%, #073AFF00 100%),
+        radial-gradient(20% 30% at 30% 1%, #8A4FFF4D 9%, #073AFF00 100%),
+        linear-gradient(320deg, #020015 42%, #291379FF 100%);
+        background-position: center;
+        background-size: 100%;
+        background-repeat: no-repeat;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+        padding: 130px 0px;
+
+        @include Article_Title;
+    }
+
+    &__article{
+        
+        &__conclusion, &__introduction{
+            padding: 100px;
+            background-size: 100% 100%;
+            background-position: 0px 0px,0px 0px,0px 0px;
+            background-image: radial-gradient(50% 50% at -12% 40%, #00FFFF45 3%, #00031700 100%),radial-gradient(34% 46% at 107% 40%, #00FFFF3D 4%, #00031700 100%),radial-gradient(75% 75% at 50% 50%, #0B0D3AFF 0%, #080A1EFF 100%);
+
+            &__content{
+                display: flex;
+                align-content: center;
+                align-items: center;
+                gap: 100px;
+
+                &__images{
+                    position: relative;
+                    margin: auto;
+
+                    img{
+                        width: 100%;
+                        margin: auto;
+                        @include PictureShadow;
+                    }
+                }
+
+                &__texte{
+                    height: 100%;
+                    line-height: 1.5rem;
+                    display: flex;
+                    flex-direction: column;
+                    gap: 30px;
+
+                    h2{
+                        @include h2;
+                        line-height: 40px;
+                        text-align: center;
+                    }
+                }
+            }
+        }
+
+        &__content{
+            width: 100%;
+
+            &__presentation {
+                padding: 100px;
+                background:$primary-color ;
+
+                &__borders{
+                    padding: 50px;
+                    position:relative;
+                    transition: 1s ease-in-out;
+
+                    &:hover {
+                    @include PictureShadow;
+
+                        &:before,
+                        &:after {
+                            width: 700px;
+                            height: 200px;
+                            
+                        }
+
+                        span.border_btm {
+                            &:before,
+                            &:after {
+                                width: 700px;
+                                height: 200px;
+                            }
+                        }
+                }
+
+                    h2{
+                        margin-bottom: 30px;
+                    }
+                    span.border_btm {
+
+                        &:after {
+                            display: block;
+                            content: "";
+                            width: 30px;
+                            height: 30px;
+                            position: absolute;
+                            bottom: -10px;
+                            right: -10px;
+                            border-bottom: 5px solid $teal-color;
+                            border-right: 5px solid $teal-color;
+                            transition: 1s ease-in-out;
+                        }
+
+                        &:before {
+                            display: block;
+                            content: "";
+                            width: 30px;
+                            height: 30px;
+                            position: absolute;
+                            bottom: -10px;
+                            left: -10px;
+                            border-bottom: 5px solid $teal-color;
+                            border-left: 5px solid $teal-color;
+                            transition: 1s ease-in-out;
+                        }
+                    }
+
+                    &:before {
+                        display: block;
+                        content: "";
+                        width: 30px;
+                        height: 30px;
+                        position: absolute;
+                        top: -10px;
+                        left: -10px;
+                        border-top: 5px solid $teal-color;
+                        border-left: 5px solid $teal-color;
+                        transition: 1s ease-in-out;
+                    }
+                    &:after {
+                        display: block;
+                        content: "";
+                        width: 30px;
+                        height: 30px;
+                        position: absolute;
+                        top: -10px;
+                        right: -10px;
+                        border-top: 5px solid $teal-color;
+                        border-right: 5px solid $teal-color;
+                        transition: 1s ease-in-out;
+                    }
+
+                }
+                
+            }
+
+            &__theory{
+                width: 100%;
+                padding: 100px;
+                background-size: 100%;
+                background-position: center;
+                background-image: 
+                    url('/altered_creatures/creature_6_opacity.png'),
+                    radial-gradient(50% 50% at -12% 40%, #00FFFF45 3%, #00031700 100%),
+                    radial-gradient(34% 46% at 107% 40%, #00FFFF3D 4%, #00031700 100%),
+                    radial-gradient(75% 75% at 50% 50%, #0B0D3AFF 0%, #080A1EFF 100%);
+                background-repeat: no-repeat;
+
+                
+                &__title{
+                    @include h2;
+                    text-align: center;
+                    margin-bottom: 100px;
+                }
+
+                &__texte{
+                    display: flex;
+                    justify-content: center;
+                    gap: 50px;
+                    line-height: 20px;
+
+                    p{
+                        width: 100%;
+                    }
+                }
+            }
+
+            &__ships{
+                background: $primary-color;
+                padding: 50px 100px;
+
+                &__box{
+                    position:relative;
+                    padding: 50px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    gap: 50px;
+                    transition: 1s;
+                    margin: 100px 0px;
+
+                    &:hover {
+                        @include PictureShadow;
+                        &:before,
+                        &:after {
+                            width: 700px;
+                            height: 400px;
+                            
+                        }
+
+                        span.border_btm {
+                        &:before,
+                        &:after {
+                            width: 700px;
+                            height: 400px;
+                        }
+                        }
+                    }
+
+                    h2{
+                        margin-bottom: 30px;
+                    }
+
+                    &:before {
+                        display: block;
+                        content: "";
+                        width: 50px;
+                        height: 50px;
+                        position: absolute;
+                        top: -10px;
+                        left: -10px;
+                        border-top: 5px solid $teal-color;
+                        border-left: 5px solid $teal-color;
+                        transition: 1s;
+                    }
+                    &:after {
+                        display: block;
+                        content: "";
+                        width: 50px;
+                        height: 50px;
+                        position: absolute;
+                        top: -10px;
+                        right: -10px;
+                        border-top: 5px solid $teal-color;
+                        border-right: 5px solid $teal-color;
+                        transition: 1s;
+                    }
+
+                    span.border_btm {
+
+                        &:after {
+                            display: block;
+                            content: "";
+                            width: 50px;
+                            height: 50px;
+                            position: absolute;
+                            bottom: -10px;
+                            right: -10px;
+                            border-bottom: 5px solid $teal-color;
+                            border-right: 5px solid $teal-color;
+                            transition: 1s;
+                        }
+
+                        &:before {
+                            display: block;
+                            content: "";
+                            width: 50px;
+                            height: 50px;
+                            position: absolute;
+                            bottom: -10px;
+                            left: -10px;
+                            border-bottom: 5px solid $teal-color;
+                            border-left: 5px solid $teal-color;
+                            transition: 1s;
+                        }
+                    }
+
+                    &__texte{
+                    width: 65%;
+                    @include Article_Text;
+                    
+
+                        h2{
+                            width: 100%;
+                            margin-bottom: 50px;
+                            @include h2;
+                            text-align: center;
+                        }
+                    }
+
+                    img{
+                        width: 35%;
+                    }
+                }
+            }
+
+            &__discovery{
+                padding: 100px;
+                border-top: 5px ;
+                display: flex;
+                align-content: center;
+                align-items: center;
+                gap: 100px;
+                background-size: 100% 100%;
+                background-position: 0px 0px,0px 0px,0px 0px;
+                background-image: radial-gradient(50% 50% at -12% 40%, #00FFFF45 3%, #00031700 100%),radial-gradient(34% 46% at 107% 40%, #00FFFF3D 4%, #00031700 100%),radial-gradient(75% 75% at 50% 50%, #0B0D3AFF 0%, #080A1EFF 100%);
+
+                &__texte{
+                    width: 70%;
+                    display: flex;
+                    flex-direction: column; 
+                    gap: 30px;
+
+                    h2{
+                        @include h2;
+                        text-align: center;
+                    }
+
+                    p{
+                        line-height: 1.5rem;
+                    }
+                }
+
+                img{
+                    width: 50%;
+                    margin: auto;
+                    box-shadow: 0px 0px 50px $teal-color;
+                }
+            }
+        }
+    }
+}
+
+
+h2{
+    @include Article_Title;
+    line-height: 120%;
+}
+
+p{
+    @include Article_Text;
+    line-height: 1.5rem;
+}
+
+img{
+    @include PictureShadow;
+}
 </style>
